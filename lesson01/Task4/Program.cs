0e4cd8e436c437a745a55c4b4548e196c28ca121
Vladislav Ponomarev <smellofblodd@yandex.ru>
2024-02-19 02:03:35 +0300

    Lesson1:Task4

---
+++ lesson01/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 20;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
